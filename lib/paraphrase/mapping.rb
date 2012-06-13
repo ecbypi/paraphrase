58987b040ed6dd848a6a5d7c5c1d70e5feb28478
Eduardo Gutierrez <edd_d@mit.edu>
2012-06-13 12:55:36 -0400

    Make Mapping options readable

--- lib/paraphrase/mapping.rb
+++ lib/paraphrase/mapping.rb
@@ -1,6 +1,6 @@
 module Paraphrase
   class Mapping
-    attr_reader :param_key, :scope
+    attr_reader :param_key, :scope, :options
 
     def initialize(mapping, options = {})
       @param_key, @scope = mapping.to_a.first
