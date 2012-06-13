d38375bd1b60c43166fa9b078c64171a086800d7
Eduardo Gutierrez <edd_d@mit.edu>
2012-06-13 10:25:03 -0400

    Mapping options should be optional

--- lib/paraphrase/mapping.rb
+++ lib/paraphrase/mapping.rb
@@ -2,7 +2,7 @@ module Paraphrase
   class Mapping
     attr_reader :param_key, :scope
 
-    def initialize(mapping, options)
+    def initialize(mapping, options = {})
       @param_key, @scope = mapping.to_a.first
       @options = options.freeze
     end
