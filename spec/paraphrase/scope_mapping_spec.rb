e6db88f043f238b4375db86ff2eb9efc0c4d6f57
Eduardo Gutierrez <edd_d@mit.edu>
2012-07-05 01:16:28 -0400

    Cleanup ScopeMapping spec

--- spec/paraphrase/scope_mapping_spec.rb
+++ spec/paraphrase/scope_mapping_spec.rb
@@ -2,48 +2,43 @@ require 'spec_helper'
 
 module Paraphrase
   describe ScopeMapping do
-    let(:scope_mapping) do
-      ScopeMapping.new :name_like, :key => :name
-    end
-    let(:query) { double('query') }
-
-    it "removes keys from options" do
-      scope_mapping.options.should_not have_key :key
+    let(:mapping) { ScopeMapping.new(:name_like, :key => :name) }
+    let(:compound_mapping) { ScopeMapping.new(:name_like, :key => [:first_name, :last_name], :require => :last_name) }
+    let(:query) do
+      query = double('query')
+      query.stub(:errors).and_return(double('errors'))
+      query
     end
 
     describe "#chain" do
       it "applies scope method to query object with values from params hash" do
         Account.should_receive(:name_like).with('Jon Snow')
-        scope_mapping.chain(query, { :name => 'Jon Snow' }, Account)
+        mapping.chain(query, { :name => 'Jon Snow' }, Account)
       end
 
       it "does nothing if values are missing" do
         Account.should_not_receive(:name_like).with('Jon Snow')
-        scope_mapping.chain(query, {}, Account)
+        mapping.chain(query, {}, Account)
       end
 
       it "adds errors to query object if missing and required" do
-        errors = double('errors')
-        query.stub(:errors).and_return(errors)
-        required_mapping = ScopeMapping.new :name_like, :key => :name, :require => true
+        required_mapping = ScopeMapping.new(:name_like, :key => :name, :require => true)
 
-        errors.should_receive(:add)
+        query.errors.should_receive(:add)
         required_mapping.chain(query, {}, Account)
       end
 
       it "passes through nil values if scope has been whitelisted" do
-        whitelisted_mapping = ScopeMapping.new :name_like, :key => :name, :allow_nil => true
+        mapping = ScopeMapping.new(:name_like, :key => :name, :allow_nil => true)
 
         Account.should_receive(:name_like).with(nil)
-        whitelisted_mapping.chain(query, {}, Account)
+        mapping.chain(query, {}, Account)
       end
     end
 
     it "can require a subset of a compound key" do
-      mapping = ScopeMapping.new :name_like, :key => [:first_name, :last_name], :require => :last_name
-
       Account.should_receive(:name_like).with(nil, 'Lannister')
-      mapping.chain(query, { :last_name => 'Lannister' }, Account)
+      compound_mapping.chain(query, { :last_name => 'Lannister' }, Account)
     end
   end
 end
