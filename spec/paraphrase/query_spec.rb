68abbc5c2331010a4b7bff0e98f218d0b7f3083f
Eduardo Gutierrez <edd_d@mit.edu>
2012-07-06 19:46:21 -0400

    Only use one AR model for tests
    Keep Account model and switch Query spec to use it

--- spec/paraphrase/query_spec.rb
+++ spec/paraphrase/query_spec.rb
@@ -5,31 +5,28 @@ module Paraphrase
 
     describe ".paraphrases" do
       it "stores the class being queried" do
-        UserSearch.paraphrases :user
-        UserSearch.source.should eq User
-      end
-
-      it "registers the query in Paraphrase.querys" do
-        Paraphrase.mapping(:user).should eq UserSearch
+        AccountSearch.paraphrases :account
+        AccountSearch.source.should eq Account
       end
     end
 
     describe ".scope" do
       it "adds information to Query.scopes" do
-        UserSearch.instance_eval do
+        AccountSearch.instance_eval do
           scope :name_like, :key => :name
         end
 
-        UserSearch.scopes.should_not be_empty
+        AccountSearch.scopes.should_not be_empty
       end
 
       it "raises an error if a scope is added twice" do
-        expect { UserSearch.instance_eval { scope :name_like, :key => :name } }.to raise_error Paraphrase::DuplicateScopeError
+        expect { AccountSearch.instance_eval { scope :name_like, :key => :name } }.to raise_error Paraphrase::DuplicateScopeError
       end
     end
 
     describe "#initialize" do
-      let(:query) { UserSearch.new(:name => 'Tyrion Lannister', :nickname => 'Half Man') }
+      let(:query) { AccountSearch.new(:name => 'Tyrion Lannister', :nickname => 'Half Man') }
+
       it "filters out params not specified in scopes" do
         query.params.should_not have_key :nickname
         query.params.should have_key :name
@@ -42,21 +39,21 @@ module Paraphrase
 
     describe "#results" do
       before :all do
-        UserSearch.instance_eval do
+        AccountSearch.instance_eval do
           scope :title_like, :key => :title, :require => true
         end
       end
 
       it "loops through scope methods and applies them to source" do
-        User.should_receive(:title_like).and_return(User.scoped)
-        User.should_receive(:name_like).and_return(User.scoped)
+        Account.should_receive(:title_like).and_return(Account.scoped)
+        Account.should_receive(:name_like).and_return(Account.scoped)
 
-        query = UserSearch.new(:name => 'Jon Snow', :title => 'Wall Watcher')
+        query = AccountSearch.new(:name => 'Jon Snow', :title => 'Wall Watcher')
         query.results
       end
 
       it "returns empty array if errors were added" do
-        query = UserSearch.new
+        query = AccountSearch.new
         query.results.should eq []
         query.errors.should_not be_empty
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,25 +11,13 @@ ActiveRecord::Base.silence do
   ActiveRecord::Migration.verbose = false
 
   ActiveRecord::Schema.define do
-    create_table(:users, :force => true) {}
     create_table(:accounts, :force => true) {}
-    create_table(:foobars, :force => true) {}
-    create_table(:people, :force => true) {}
   end
-end
-
-class Person < ActiveRecord::Base
-end
-
-class Foobar < ActiveRecord::Base
 end
 
 class Account < ActiveRecord::Base
   extend Paraphrase::Syntax
 end
 
-class User < ActiveRecord::Base
-end
-
-class UserSearch < Paraphrase::Query
+class AccountSearch < Paraphrase::Query
 end
