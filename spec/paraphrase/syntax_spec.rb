d7457c0d7f0cdfce07074c0dd12e6fbdc55ed349
Eduardo Gutierrez <edd_d@mit.edu>
2012-08-07 21:02:14 -0400

    Correct description in Syntax spec

--- spec/paraphrase/syntax_spec.rb
+++ spec/paraphrase/syntax_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Paraphrase
   describe Syntax do
     describe ".register_mapping" do
-      it "forwards to Paraphrase.register" do
+      it "creates new sublcass of Query" do
         Account.register_mapping {}
         Account.paraphraser.should_not be_nil
       end
