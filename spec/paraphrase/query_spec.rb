f50f65fc33b75fdc0da60234227808f19a45003b
Eduardo Gutierrez <edd_d@mit.edu>
2012-09-14 01:16:56 -0400

    Cleanup Query specs
    Remove inter-test dependencies

--- spec/paraphrase/query_spec.rb
+++ spec/paraphrase/query_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 module Paraphrase
   describe Query do
-    subject { AccountQuery }
-    let(:query) { Account.paraphrase(:name => 'Tyrion Lannister', :nickname => 'Half Man') }
-
     describe ".map" do
+      subject { Class.new(Query) }
+
       it "adds information to Query.mappings" do
         subject.map :name_like, :to => :name
 
@@ -13,11 +12,21 @@ module Paraphrase
       end
 
       it "raises an error if a scope is added twice" do
+        subject.map :name_like, :to => :name
+
         expect { subject.map :name_like, :to => :name }.to raise_error Paraphrase::DuplicateScopeError
       end
     end
 
     describe "on initialization" do
+      let(:query) do
+        klass = Class.new(Query) do
+          map :name_like, :to => :name
+        end
+
+        klass.new({ :name => '', :nickname => '' }, Account)
+      end
+
       it "filters out params not specified in mappings" do
         query.params.should_not have_key :nickname
         query.params.should have_key :name
@@ -29,20 +38,23 @@ module Paraphrase
     end
 
     describe "#results" do
-      before :all do
-        subject.map :title_like, :to => :title, :require => true
+      let(:klass) do
+        Class.new(Query) do
+          map :name_like, :to => :name
+          map :title_like, :to => :title, :require => true
+        end
       end
 
       it "loops through scope methods and applies them to source" do
         Account.should_receive(:title_like).and_return(Account.scoped)
         Account.should_receive(:name_like).and_return(Account.scoped)
 
-        query = Account.paraphrase(:name => 'Jon Snow', :title => 'Wall Watcher')
+        query = klass.new({ :name => 'Jon Snow', :title => 'Wall Watcher'}, Account)
         query.results
       end
 
       it "returns empty array if inputs were missing and required" do
-        query = Account.paraphrase({})
+        query = klass.new({}, Account)
         query.results.should eq []
       end
     end
