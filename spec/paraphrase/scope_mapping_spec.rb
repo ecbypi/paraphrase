0ff44dc48cb4362f50b0bc8ebd10e4426f27897b
Eduardo Gutierrez <edd_d@mit.edu>
2012-09-14 01:21:12 -0400

    Cleanup ScopeMapping specs
    Remove inter-test dependencies

--- spec/paraphrase/scope_mapping_spec.rb
+++ spec/paraphrase/scope_mapping_spec.rb
@@ -2,36 +2,45 @@ require 'spec_helper'
 
 module Paraphrase
   describe ScopeMapping do
-    let(:mapping) { ScopeMapping.new(:name_like, :to => :name) }
-    let(:compound_mapping) { ScopeMapping.new(:name_like, :to => [:first_name, :last_name], :require => :last_name) }
-    let(:query) { Account.scoped }
 
     describe "#chain" do
-      it "applies scope method to query object with values from params hash" do
+      let(:mapping) { ScopeMapping.new(:name_like, :to => :name) }
+
+      it "applies scope method to Account.scoped object with values from params hash" do
         Account.should_receive(:name_like).with('Jon Snow')
-        mapping.chain({ :name => 'Jon Snow' }, query)
+
+        mapping.chain({ :name => 'Jon Snow' }, Account.scoped)
       end
 
       it "does nothing if values are missing" do
         Account.should_not_receive(:name_like).with('Jon Snow')
-        mapping.chain({}, query)
+
+        mapping.chain({}, Account.scoped)
       end
 
       it "passes through nil values if scope has been whitelisted" do
         mapping = ScopeMapping.new(:name_like, :to => :name, :whitelist => true)
 
         Account.should_receive(:name_like).with(nil)
-        mapping.chain({}, query)
+
+        mapping.chain({}, Account.scoped)
       end
     end
 
-    it "can require a subset of a compound key" do
-      Account.should_receive(:name_like).with(nil, 'Lannister')
-      compound_mapping.chain({ :last_name => 'Lannister' }, query)
-    end
+    describe "compound keys" do
+      let(:compound_mapping) do
+        ScopeMapping.new(:name_like, :to => [:first_name, :last_name], :require => :last_name)
+      end
 
-    it "whitelists the the non-required keys of a compound key" do
-      compound_mapping.whitelist.include?(:first_name).should be_true
+      it "can require a subset of a compound key" do
+        Account.should_receive(:name_like).with(nil, 'Lannister')
+
+        compound_mapping.chain({ :last_name => 'Lannister' }, Account.scoped)
+      end
+
+      it "whitelists the the non-required keys of a compound key" do
+        compound_mapping.whitelist.include?(:first_name).should be_true
+      end
     end
   end
 end
