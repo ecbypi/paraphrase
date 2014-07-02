4063f799e2a35e6167893c51beb388728c4ad85e
Eduardo Gutierrez <eduardo@vermonster.com>
2014-07-02 09:52:35 -0400

    Fix `require` typo in Railtie

--- lib/paraphrase/rails.rb
+++ lib/paraphrase/rails.rb
@@ -4,7 +4,7 @@ module Paraphrase
   class Railtie < Rails::Railtie
     initializer 'paraphrase.extend_active_record' do
       ActiveSupport.on_load :active_record do
-        require 'paraphrase/synstax'
+        require 'paraphrase/syntax'
 
         extend Paraphrase::Syntax
       end
